6c499a69ee4b9ef8bc2261ce4765a99db1086a12
Mariusz Nowak <mariusz@medikoo.com>
2014-07-17 16:34:19 +0200

    Fix vendor prefix

--- rules/box-shadow.js
+++ rules/box-shadow.js
@@ -3,7 +3,7 @@
 var re = /([;\{]\s*)box-shadow\s*:\s*([a-zA-Z% 0-9\-\.]+)(\s*[;\}])/g
 
   , replacement = '-webkit-box-shadow: $2;' +
-	' moz-box-shadow: $2;' +
+	' -moz-box-shadow: $2;' +
 	' box-shadow: $2';
 
 module.exports = exports = function (str) {
