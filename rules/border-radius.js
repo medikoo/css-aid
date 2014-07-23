7b3c33464f2cce3806b77ac069bb1594734807fe
Mariusz Nowak <mariusz@medikoo.com>
2014-07-23 14:36:16 +0200

    Fix border-radius replace

--- rules/border-radius.js
+++ rules/border-radius.js
@@ -2,9 +2,9 @@
 
 var re = /([;\{]\s*)border-radius\s*:\s*([a-zA-Z% 0-9\-\.]+)(\s*[;\}])/g
 
-  , replacement = '-webkit-border-radius: $2;' +
-	' moz-border-radius: $2;' +
-	' khtml-border-radius: $2;' +
+  , replacement = '-moz-border-radius: $2;' +
+	' -webkit-border-radius: $2;' +
+	' -khtml-border-radius: $2;' +
 	' border-radius: $2';
 
 module.exports = exports = function (str) {
